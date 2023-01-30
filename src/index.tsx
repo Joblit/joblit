8be863c6b32a755a141222d99002a2c84ebda618
Rachel Cheman <rachelcheman@gmail.com>
2023-01-30 16:33:28 -0500

    in progress.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import App from './App';
+// import { createRoot } from 'react-dom/client';
 
-const root: any = createRoot(document.getElementById('root') as HTMLElement);
-// root.render(<App />);
-if (!root) throw new Error('Failed to find the root element');
-root.render(<h1>Something from index</h1>);
+// const root: any = createRoot(document.getElementById('root') as HTMLElement);
+// // root.render(<App />);
+// if (!root) throw new Error('Failed to find the root element');
+// root.render(<h1>Something from index</h1>);
 
 // const domNode = document.getElementById('root');
 // const root = createRoot(domNode as HTMLElement);
@@ -13,16 +13,16 @@ root.render(<h1>Something from index</h1>);
 // root.render(<App />);
 
 // example fromRedux toolkit quick-start
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
-// import { store } from './app/store'
-// import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
