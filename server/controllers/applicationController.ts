dbb8c00dd96816d0bda71ddd6515440b1e6b3b3d
kimballcaroline <kimballcaroline@gmail.com>
2023-01-30 18:34:55 -0500

    Update controllers

--- server/controllers/applicationController.ts
+++ server/controllers/applicationController.ts
@@ -1,3 +1,19 @@
-const db = require("../models/joblitModels");
+// const db = require("../models/joblitModels");
 
-const applicationController = {};
+// const applicationController = {};
+
+// applicationController.createApplication = (req, res, next) => {};
+
+// module.exports = applicationController;
+
+import { Request, Response, NextFunction } from "express";
+
+export const applicationController = {
+  createApplication: (req: Request, res: Response, next: NextFunction) => {},
+
+  getAllApplications: (req: Request, res: Response, next: NextFunction) => {},
+
+  deleteApplication: (req: Request, res: Response, next: NextFunction) => {},
+
+  updateApplication: (req: Request, res: Response, next: NextFunction) => {},
+};
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -3,3 +3,5 @@ const db = require("../models/joblitModels");
 const userController = {};
 
 userController.getUser = (req: Request, res: Response, next: any) => {};
+
+module.exports = userController;
