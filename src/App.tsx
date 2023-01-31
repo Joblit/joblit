816f4fa05bb26f2d07789c3cd8d7f0ec520f85a8
Rachel Cheman <rachelcheman@gmail.com>
2023-01-31 10:13:43 -0500

    progress on adding login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddJobModal from './components/AddJobModal';
 import JobModal from './components/JobModal';
+import Login from './components/Login';
 
 // Temp values for testing
 const user = 'testUserId';
@@ -20,6 +21,7 @@ const jobModalProps = {
 const App = () => {
   return (
     <div>
+      <Login />
       <h1>Macon</h1>
       <img src='../assets/joblit_no_background.png' />
       <JobModal
