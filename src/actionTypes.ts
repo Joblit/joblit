19e134bb97632deab3b7d659373bba36f501681b
Rachel Cheman <rachelcheman@gmail.com>
2023-01-31 10:55:31 -0500

    progress

---
+++ src/actionTypes.ts
@@ -0,0 +1,21 @@
+// action type constants to be exported
+
+// Related to status columns
+export const ADD_COLUMN = 'ADD_COLUMN';
+
+export const DELETE_COLUMN = 'DELETE_COLUMN';
+
+// Related to login
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const LOGOUT = 'LOGOUT';
+
+export const SET_MESSAGE = 'SET_MESSAGE';
+
+export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,16 @@
+// createStore is depricated for redux, it recommends redux toolkit for configuring the store
 import { configureStore } from '@reduxjs/toolkit';
-import columnReducer from './reducers/columnsSlice';
-// import in the reducers we will be using
+import columnsSlice from './reducers/columnsSlice';
+// // import in the reducers we will be using
 
 export const store = configureStore({
   reducer: {
-    column: columnReducer,
-    // users: usersReducer,
-    // applications: applicationsReducer,
+    // I don't know what reducers we actually want here
+    columns: columnsSlice,
   },
 });
 
-// Infer the RootState and AppDispatch types from the store itself
+// // Infer the RootState and AppDispatch types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {users: UsersState, applications: applicationsState}
+// // Inferred type: {users: UsersState, applications: applicationsState}
 export type AppDispatch = typeof store.dispatch;
