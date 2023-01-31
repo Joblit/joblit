8bc553f1fe0bbdb058000435d4f6ac87a3d4900c
kimballcaroline <kimballcaroline@gmail.com>
2023-01-30 20:00:36 -0500

    Add To Do notes to joblitModels

--- server/models/joblitModels.ts
+++ server/models/joblitModels.ts
@@ -16,10 +16,10 @@ module.exports = {
 /* 
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
-	first_name VARCHAR UNIQUE NOT NULL,
-  last_name VARCHAR UNIQUE NOT NULL,
+	first_name VARCHAR UNIQUE NOT NULL, ---> Should not be unique??
+  last_name VARCHAR UNIQUE NOT NULL, ---> Should not be unique??
 	password VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
+	email VARCHAR NOT NULL, ----> SHOULD be unique??
 	created_on TIMESTAMP
 );
 */
@@ -46,12 +46,15 @@ Add fulltime/part-time and tags to applications
 Build salary as ranges with radio buttons
  */
 
+//Create user query
 // INSERT INTO users (first_name, last_name, password, email)
 // VALUES ('Macon', 'Bacon', 'crispy', 'chrispbacon@gmail.com');
 
+//Create application query
 // INSERT INTO applications (user_id, title)
 // VALUES (1, 'Macons Dream Job');
 
+//Get user plus applications query
 // SELECT u.*, app.*
 // FROM users u
 // INNER JOIN applications app
