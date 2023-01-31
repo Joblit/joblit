0fb70053766486a9b4a93f8f52dacd4053b4059c
Chase Benjamin <chasebenj19@gmail.com>
2023-01-31 16:23:41 -0500

    finished user controllers

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,102 +1,108 @@
 import { Request, Response, NextFunction } from "express";
-// const bcrypt = require("bcrypt");
-// const SALT_WORK_FACTOR: number = 10;
+import { ServerError } from "../../types";
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR: number = 10;
 
 const db = require("../models/joblitModels");
 
 export const userController = {
-  createUser: (req: Request, res: Response, next: NextFunction) => {
-    console.log("entered userController.createUser");
-    // bcrypt.genSalt(SALT_WORK_FACTOR, (error: any, salt: any) => {
-    //   if (error) return next(error);
-    //   bcrypt.hash(req.body.password, salt, function (error: any, hash: any) {
-    //     if (error) return next(error);
-    //     req.body.password = hash;
-    const addUserQuery = {
+  createUser: async (req: Request, res: Response, next: NextFunction) => {
+
+    try{
+      const {first_name, last_name, password, email} = req.body;
+
+     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+     const hash = await bcrypt.hash(password, salt);
+
+      const addUserQuery = {
       text: "INSERT INTO users (first_name, last_name, password, email) VALUES ($1, $2, $3, $4) RETURNING *",
       values: [
-        req.body.first_name,
-        req.body.last_name,
-        req.body.password,
-        req.body.email,
+        first_name,
+        last_name,
+        hash,
+        email,
       ],
     };
-    db.query(addUserQuery, (error: any, data: any) => {
-      if (error) return next({ error });
-      res.status(200);
-      console.log("data from createUser response", data);
-      res.locals.response = data;
+
+      const newUser = await db.query(addUserQuery);
+      res.locals.newUser = newUser.rows[0];
       return next();
-    });
-    //   });
-    // });
+    } catch (error) {
+      return next({
+        log: 'Error while creating user',
+        status: 400,
+        message: {
+          err: 'Error from userController.createUser, proper inputs not received'
+        }
+      });
+    }
   },
 
-  verifyUser: (req: Request, res: Response, next: NextFunction) => {
-    // const query = {
-    //   text: "SELECT user_id,password FROM users WHERE username = $1",
-    //   values: [req.body.username],
-    // };
-    // db.query(query, (err, data) => {
-    //   if (err) return res.status(404).send();
-    //   if (data.rowcount === 0) return res.status(401).send();
-    //   password = data.rows[0].password;
-    //   user_id = data.rows[0].user_id;
-    //   bcrypt.compare(req.body.password, password).then((result) => {
-    //     if (!result) {
-    //       console.log("=====from SERVER rehash DIDNT MATCH");
-    //       res.status(401).send();
-    //     } else {
-    //       console.log("=====from SERVER rehash WORKED");
-    //       res.locals.user_id = user_id;
-    //       res.status(200);
-    //       next();
-    //     }
-    //   });
-    // });
+  verifyUser: async (req: Request, res: Response, next: NextFunction) => {
+    try{
+      const { email, password } = req.body;
+
+      const findUserQuery = {
+           text: "SELECT * FROM users WHERE email = $1",
+           values: [email],
+         };
+       const verifiedUser = await db.query(findUserQuery);
+       const pwCheck = await bcrypt.compare(password, verifiedUser.rows[0].password);
+       if (!pwCheck) {
+               throw new Error('Password is incorrect');
+             } else {
+              res.locals.user_id = {user_id: verifiedUser.rows[0].user_id};
+              return next();
+            }
+    } catch (error) {
+      return next({
+        log: 'Error while verifying user',
+        status: 400,
+        message: {
+          err: `Error from userController.verifyUser, email or password is incorrect, ${error}`
+        }
+      });
+    }
   },
 
-  //   userController.verifyUser = async (req, res, next) => {
-  //   try {
-  //     const { email, password } = req.body;
-  //     const userData = await User.find({ email: email });
-  //     const pwCheck = await bcrypt.compare(password, userData[0].password);
-  //     if (!pwCheck) {
-  //       throw new Error('Password is incorrect');
-  //     } else {
-  //       const { RoleARN, region, _id } = userData[0];
-  //       res.locals.newUser = { RoleARN, region, _id };
-  //       return next();
-  //     }
-  //   } catch (err) {
-  //     return next({
-  //       log: `Error in userController.verifyUser. Details: ${err}`,
-  //       message: { err: 'An error occurred in userController.verifyUser' },
-  //     });
-  //   }
-  // };
 
-  deleteUser: (req: Request, res: Response, next: NextFunction) => {
-    // const deleteUserQuery = {
-    //   text: "DELETE FROM users WHERE user_id=$1",
-    //   values: [req.body.user_id],
-    // };
-    // db.query(deleteUserQuery, (err, data) => {
-    //   if (err) return next({ err });
-    //   console.log(data);
-    //   return next();
-    // });
+  deleteUser: async (req: Request, res: Response, next: NextFunction) => {
+    try{
+      const { user_id } = res.locals;
+         const deleteUserQuery = {
+        text: "DELETE FROM users WHERE user_id=$1",
+        values: [user_id],
+     };
+      await db.query(deleteUserQuery);
+
+      return next();
+    } catch (error) {
+      return next({
+        log: 'Error while deleting user',
+        status: 400,
+        message: {
+          err: `Error from userController.verifyUser, user could not be deleted ${error}`
+        }
+      });
+    }
   },
 
   updateUser: (req: Request, res: Response, next: NextFunction) => {},
 
-  getAllUsers: (req: Request, res: Response, next: NextFunction) => {
-    console.log("entered getallusers in usercontroller");
-    db.query("SELECT * FROM users", (err: any, data: any) => {
-      if (err) return next({ err });
-      console.log(data);
-      res.locals.user = data;
+  getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log("entered getallusers in usercontroller");
+      const allUsers = await db.query("SELECT * FROM users");
+      res.locals.allUsers = allUsers;
       return next();
-    });
+    }  catch (error) {
+      return next({
+        log: 'Error while getting users',
+        status: 400,
+        message: {
+          err: `Error from userController.getAllUsers, query failed ${error}`
+        }
+      });
+    }
   },
 };
