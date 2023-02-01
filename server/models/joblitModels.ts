a2ccca34822e9ade95cd972585e861913461ac69
Chase Benjamin <chasebenj19@gmail.com>
2023-01-31 19:53:56 -0500

    added sessions table

--- server/models/joblitModels.ts
+++ server/models/joblitModels.ts
@@ -25,20 +25,22 @@ CREATE TABLE users (
 );
 */
 
-/* 
-CREATE TABLE applications (
-  application_id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL,
-	title VARCHAR NOT NULL,
-  company VARCHAR,
-	salary INT,
-	description VARCHAR,
-  contact_name VARCHAR,
-  contact_email VARCHAR,
-  location VARCHAR,
-  status VARCHAR,
-	created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*
+  CREATE TABLE applications (
+    application_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    companyName VARCHAR,
+    jobTitle VARCHAR,
+    jobDescription VARCHAR,
+    location VARCHAR,
+    applicationDate VARCHAR,
+    salaryRange VARCHAR,
+    contactPerson VARCHAR,
+    contactEmail VARCHAR,
+    benefits VARCHAR,
+    notes VARCHAR,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
 */
 
 /* 
@@ -54,17 +56,3 @@ STRETCH GOALS:
 Add fulltime/part-time and tags to applications
 Build salary as ranges with radio buttons
  */
-
-//Create user query
-// INSERT INTO users (first_name, last_name, password, email)
-// VALUES ('Macon', 'Bacon', 'crispy', 'chrispbacon@gmail.com');
-
-//Create application query
-// INSERT INTO applications (user_id, title)
-// VALUES (1, 'Macons Dream Job');
-
-//Get user plus applications query
-// SELECT u.*, app.*
-// FROM users u
-// INNER JOIN applications app
-// ON u.user_id=app.user_id;
