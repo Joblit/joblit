4cc9b412c1d9a69fdda42717dd37c3f5685cae8a
Rachel Cheman <rachelcheman@gmail.com>
2023-02-01 16:14:47 -0500

    Adds radio buttons to update the status.

--- src/components/JobModal.tsx
+++ src/components/JobModal.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useAppSelector } from '../redux/hooks';
+import React, { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { RootState } from '../redux/store';
+import { editStatus } from '../redux/jobSlice';
 
 type jobModalProps = {
   application_id: number;
@@ -8,26 +9,34 @@ type jobModalProps = {
 
 // Job modal that shows the details of a particular job
 function JobModal(props: jobModalProps) {
-  const {application_id} = props;
+  const { application_id } = props;
+  const [statusState, setStatus] = useState('Applied');
+  const dispatch = useAppDispatch();
   // Get the job details of the passed in jobId from state
   // const jobDetails = useAppSelector(
   //   (state: RootState) => state.jobs.applications[application_id]
   // );
   //   console.log('jobDetails from JobModal: ', jobDetails);
+  function onStatusChange(event: any) {
+    setStatus(event.target.value);
+    const copy = JSON.parse(JSON.stringify(jobDetails));
+    copy.status = statusState;
+    dispatch(editStatus(copy));
+  }
 
-    const applications = useAppSelector(
+  const applications = useAppSelector(
     (state: RootState) => state.jobs.applications
   );
 
-    let jobIndex: number;
+  let jobIndex: number;
 
-    applications.forEach((job, index)=> {
-      if(job.application_id === application_id) {
-        jobIndex = index;
+  applications.forEach((job, index) => {
+    if (job.application_id === application_id) {
+      jobIndex = index;
     }
   });
 
-      const jobDetails = useAppSelector(
+  const jobDetails = useAppSelector(
     (state: RootState) => state.jobs.applications[jobIndex]
   );
 
@@ -50,6 +59,49 @@ function JobModal(props: jobModalProps) {
           <p>Contact Email: {jobDetails.contactemail}</p>
           <p>Benefits: {jobDetails.benefits}</p>
           <p>Notes: {jobDetails.notes}</p>
+          <form>
+            <fieldset>
+              <div>
+                <input
+                  type='radio'
+                  id='statusActive'
+                  name='status'
+                  value='Active'
+                  checked={status === 'Active'}
+                  onChange={onStatusChange}
+                />
+                <label htmlFor='statusActive'>Active</label>
+                <input
+                  type='radio'
+                  id='statusInterview'
+                  name='status'
+                  value='Interview'
+                  checked={status === 'Interview'}
+                  onChange={onStatusChange}
+                />
+                <label htmlFor='statusInterview'>Interview</label>
+                <input
+                  type='radio'
+                  id='statusOffer'
+                  name='status'
+                  value='Offer'
+                  checked={status === 'Offer'}
+                  onChange={onStatusChange}
+                />
+                <label htmlFor='statusOffer'>Offer</label>
+                <input
+                  type='radio'
+                  id='statusRejected'
+                  name='status'
+                  value='Rejected'
+                  checked={status === 'Rejected'}
+                  onChange={onStatusChange}
+                />
+                <label htmlFor='statusRejected'>Rejected</label>
+                {/* <input type='submit' className='btn' value='Change Status' /> */}
+              </div>
+            </fieldset>
+          </form>
           <div className='modal-action'>
             <label htmlFor={`jobModal${application_id}`} className='btn'>
               Close
